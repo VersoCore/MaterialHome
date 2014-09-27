417d4994c4c3927aec4e4b59d3fc40b561284dcc
takahirom <takam.dev@gmail.com>
2014-09-27 23:22:23 +0900

    first commit

---
+++ app/src/main/java/com/kogitune/launcher3/util/DebugJudge.java
@@ -0,0 +1,49 @@
+package com.kogitune.launcher3.util;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Log;
+
+import java.io.ByteArrayInputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.security.auth.x500.X500Principal;
+
+import hugo.weaving.DebugLog;
+
+
+/**
+ * Created by takam on 2014/09/26.
+ */
+
+public class DebugJudge {
+    private static final X500Principal DEBUG_DN = new X500Principal("CN=Android Debug,O=Android,C=US");
+
+    public static boolean isDebuggable(Context ctx, String packageName) {
+        boolean debuggable = false;
+
+        try {
+            PackageInfo pinfo = ctx.getPackageManager().getPackageInfo(packageName, PackageManager.GET_SIGNATURES);
+            Signature signatures[] = pinfo.signatures;
+
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+            for (int i = 0; i < signatures.length; i++) {
+                ByteArrayInputStream stream = new ByteArrayInputStream(signatures[i].toByteArray());
+                X509Certificate cert = (X509Certificate) cf.generateCertificate(stream);
+                debuggable = cert.getSubjectX500Principal().equals(DEBUG_DN);
+                if (debuggable)
+                    break;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            //debuggable variable will remain false
+        } catch (CertificateException e) {
+            //debuggable variable will remain false
+        }
+        return debuggable;
+    }
+}
